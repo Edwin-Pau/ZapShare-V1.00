91c7ffd11bc77d2f1bcd1f6221f11be6838b4f01
Brian White <barwhite7@gmail.com>
2019-03-29 08:42:59 -0700

    Profile changes

--- js/profile.js
+++ js/profile.js
@@ -1,23 +1,23 @@
+function updateProfile(){
+    var firebase = app_firebase;
+    var database = firebase.database();
+    function myFunction(user){
+        firebase.database().ref("users/"+user.uid).update(
+		{
+         "name":document.getElementById("firstname").value, 
+        });
+    };
+    
+};
+
 function editProfile() {
   document.getElementById("summary-background").style.display = "none";
   document.getElementById("summary-background-edit").style.display = "block";
-}
+};
 
 function submitChanges() {
   updateProfile();
   document.getElementById("summary-background").style.display = "block";
   document.getElementById("summary-background-edit").style.display = "hidden";
-}
-
-function updateProfile(){
-    var firebase = app_firebase;
-    
-    firebase.auth().onAuthStateChanged(function(user){
-        firebase.database().ref("users/"+user.uid).update(
-		{
-        name:document.getElementById("firstname").value, 
-        email:user.email
-        });
-    });
-    
 };
+
