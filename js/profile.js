acc4b86bb6b3bfb600e74f106f04c6ef0a1d559f
Brian White <barwhite7@gmail.com>
2019-04-03 19:06:15 -0700

    Added functionality to the profile page and css

--- js/profile.js
+++ js/profile.js
@@ -1,17 +1,16 @@
+//Edit profile function that switches the content of the main div.
 function editProfile() {
   document.getElementById("summary-background").style.display = "none";
   document.getElementById("summary-background-edit").style.display = "block";
 }
 
+//Submit changes fucntion that writes the changes to the databaseand then switches the main div back.
 function submitChanges() {
   var firebase = app_firebase;
   var firstname = document.getElementById('firstname').value;    
   var lastname = document.getElementById('lastname').value;
   var address = document.getElementById('address').value;
   var city = document.getElementById('city').value;
-
-    // for the current user
-    // create this user node in the datebase
 
     firebase.auth().onAuthStateChanged(function(user){
         firebase.database().ref("users/"+user.uid).update(
@@ -24,19 +23,72 @@ function submitChanges() {
         "city":city
         });
     });
-    
   document.getElementById("summary-background").style.display = "block";
-  document.getElementById("summary-background-edit").style.display = "hidden";
+  document.getElementById("summary-background-edit").style.display = "none";
 }
 
 
 
-
-
-// Get a reference to the database service
-var database = firebase.database();
-var userId = firebase.auth().currentUser.uid;
-return firebase.database().ref('/users/' + userId).once('value').then(function(snapshot) {
-  var username = (snapshot.val() && snapshot.val().name) || 'name';
-  document.getElementById("username").innerHTML = username;
+//The following methods read the users info from the database.
+firebase.auth().onAuthStateChanged(function(user){
+var name = document.getElementById("dbusername");
+	   var db = firebase.database().ref().child("users/"+user.uid + "/name");
+	   db.on ("value", snap => name.innerText = snap.val());
 });
+firebase.auth().onAuthStateChanged(function(user){
+var name = document.getElementById("username");
+	   var db = firebase.database().ref().child("users/"+user.uid + "/name");
+	   db.on ("value", snap => name.innerText = snap.val());
+});
+
+firebase.auth().onAuthStateChanged(function(user){
+var first = document.getElementById("dbfirstname");
+	   var db = firebase.database().ref().child("users/"+user.uid + "/firstname");
+	   db.on ("value", snap => first.innerText = snap.val());
+});
+
+firebase.auth().onAuthStateChanged(function(user){
+var last = document.getElementById("dblastname");
+	   var db = firebase.database().ref().child("users/"+user.uid + "/lastname");
+	   db.on ("value", snap => last.innerText = snap.val());
+});
+
+firebase.auth().onAuthStateChanged(function(user){
+var address = document.getElementById("dbaddress");
+	   var db = firebase.database().ref().child("users/"+user.uid + "/address");
+	   db.on ("value", snap => address.innerText = snap.val());
+});
+
+firebase.auth().onAuthStateChanged(function(user){
+var city = document.getElementById("dbcity");
+	   var db = firebase.database().ref().child("users/"+user.uid + "/city");
+	   db.on ("value", snap => city.innerText = snap.val());
+});
+
+//The following methods read the users info from the database into the value of the inputs.
+firebase.auth().onAuthStateChanged(function(user){
+var first = document.getElementById("firstname");
+	   var db = firebase.database().ref().child("users/"+user.uid + "/firstname");
+	   db.on ("value", snap => first.value = snap.val());
+});
+
+firebase.auth().onAuthStateChanged(function(user){
+var last = document.getElementById("lastname");
+	   var db = firebase.database().ref().child("users/"+user.uid + "/lastname");
+	   db.on ("value", snap => last.value = snap.val());
+});
+
+firebase.auth().onAuthStateChanged(function(user){
+var address = document.getElementById("address");
+	   var db = firebase.database().ref().child("users/"+user.uid + "/address");
+	   db.on ("value", snap => address.value = snap.val());
+});
+
+firebase.auth().onAuthStateChanged(function(user){
+var city = document.getElementById("city");
+	   var db = firebase.database().ref().child("users/"+user.uid + "/city");
+	   db.on ("value", snap => city.value = snap.val());
+});
+
+
+
